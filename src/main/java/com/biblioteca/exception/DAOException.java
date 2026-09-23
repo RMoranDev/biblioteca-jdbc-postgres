ed41b56f68e8d41651c151eaed1c1de94cfeaadb
RMoranDev <ricardo.moranc2@gmail.com>
2026-09-23 12:20:11 -0300

    feat: add custom DAOException

---
+++ src/main/java/com/biblioteca/exception/DAOException.java
@@ -0,0 +1,12 @@
+package com.biblioteca.exception;
+
+public class DAOException extends RuntimeException {
+
+    public DAOException(String mensagem, Throwable causa) {
+        super(mensagem, causa);
+    }
+
+    public DAOException(String mensagem) {
+        super(mensagem);
+    }
+}
