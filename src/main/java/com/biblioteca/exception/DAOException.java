6ccc7fa164759b001b3c4504224289238b178716
RMoranDev <ricardo.moranc2@gmail.com>
2026-09-23 12:23:16 -0300

    feat: add serialVersionUID

--- src/main/java/com/biblioteca/exception/DAOException.java
+++ src/main/java/com/biblioteca/exception/DAOException.java
@@ -2,11 +2,17 @@ package com.biblioteca.exception;
 
 public class DAOException extends RuntimeException {
 
+    private static final long serialVersionUID = 1L;
+
     public DAOException(String mensagem, Throwable causa) {
         super(mensagem, causa);
     }
 
     public DAOException(String mensagem) {
         super(mensagem);
+    }
+
+    public DAOException(Throwable causa) {
+        super(causa);
     }
 }
